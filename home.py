da7ba2adf6564f1c14476723f394855ebbbf4e63
yassine guenidi <80115933+yassineguenidi@users.noreply.github.com>
2025-06-15 00:10:47 +0200

    Update home.py

--- home.py
+++ home.py
@@ -26,26 +26,24 @@
 
 # acceuil() 
 
-
 import streamlit as st
+
+# <-- ceci DOIT être la première commande streamlit, tout en haut du fichier
+st.set_page_config(page_title="Cv Manipulation", page_icon="chart_with_upwards_trend", layout="wide")
+
 from acceuil import principale
 from mainInvoice import mainInvoice
 from mainCv import mainCV
-
-# set_page_config tout en haut, hors fonction
-st.set_page_config(page_title="Cv Manipulation", page_icon="chart_with_upwards_trend", layout="wide")
 
 def acceuil():
     menu = ["Page Acceuil", "Invoice Parser", "Parser cv"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Page Acceuil":
-        print("acceuil")
         principale()
     elif choice == "Parser cv":
         mainCV()
     elif choice == "Invoice Parser":
         mainInvoice()
-        print("invoice")
 
 acceuil()
