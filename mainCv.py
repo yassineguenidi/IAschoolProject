49ea3a6676d5878ecaa4fd72822c3e667d3ce2b3
yassine guenidi <80115933+yassineguenidi@users.noreply.github.com>
2025-06-15 20:19:22 +0200

    Update mainCv.py

--- mainCv.py
+++ mainCv.py
@@ -15,7 +15,8 @@ import sys
 # import cv2 as cv
 sys.path.append('./yolov5')
 
-from models.common import DetectMultiBackend 
+# from models.common import DetectMultiBackend 
+from yolov5.models.common import DetectMultiBackend
 
 from scipy.ndimage import median_filter
 
