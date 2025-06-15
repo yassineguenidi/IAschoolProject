83d9e0082ba95b74a0e70f29707785f42e59fb05
yassine guenidi <80115933+yassineguenidi@users.noreply.github.com>
2025-06-15 04:52:40 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,39 +1,10 @@
-# import streamlit as st
-
-from acceuil import principale
-from mainInvoice import mainInvoice
-from mainCv import mainCV
-
-
-# def acceuil():
-
-#     st.set_page_config(page_title="Cv Manipulation", page_icon="chart_with_upwards_trend", layout="wide")
-#     acceuil = ["Page Acceuil", "Invoice Parser", "Parser cv"]
-#     choice = st.sidebar.selectbox("Menu", acceuil)
-
-#     if choice == "Page Acceuil":
-#         print("acceuil")
-#         principale()
-
-#     elif choice == "Parser cv":
-#         mainCV()
-#     elif choice == "Invoice Parser":
-#         mainInvoice()
-#         print("invoice")
-
-
-
-
-# acceuil() 
-
 import streamlit as st
-
-# <-- ceci DOIT être la première commande streamlit, tout en haut du fichier
 st.set_page_config(page_title="Cv Manipulation", page_icon="chart_with_upwards_trend", layout="wide")
 
 from acceuil import principale
 from mainInvoice import mainInvoice
 from mainCv import mainCV
+
 
 def acceuil():
     menu = ["Page Acceuil", "Invoice Parser", "Parser cv"]
