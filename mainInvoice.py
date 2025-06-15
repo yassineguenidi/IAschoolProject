2b4de5ae9c63ab9cb27725660baf6a2fac1f4a42
yassine guenidi <80115933+yassineguenidi@users.noreply.github.com>
2025-06-15 13:37:53 +0200

    Update mainInvoice.py

--- mainInvoice.py
+++ mainInvoice.py
@@ -83,7 +83,7 @@ def draw_boxes(image, detections):
 def preprocess_cropped_image(cropped_image):
     img_array = np.array(cropped_image)
 
-    gray = cv.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
 
     filtered = cv.medianBlur(gray, 3)
 
